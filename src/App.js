54a87bb9ea68140396b8ad97e8cc3c228bbf79a5
Gustavo Saiani <gustavo@poe.ma>
2019-12-03 17:54:44 -0300

    Create new post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
 
-const user = 'Gustavo Saiani'
-const posts = [
+const defaultPosts = [
   {
     title: 'React Hooks',
     content: 'arst arst',
@@ -18,11 +17,14 @@ const posts = [
 ]
 
 export default function App () {
+  const [ user, setUser ] = useState('')
+  const [ posts, setPosts ] = useState(defaultPosts)
+
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
       <br />
-      <CreatePost user={user} />
+      {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
--- src/post/CreatePost.js
+++ src/post/CreatePost.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function CreatePost ({ user }) {
+export default function CreatePost ({ user, posts, setPosts }) {
+  const [ title, setTitle ] = useState('')
+  const [ content, setContent ] = useState('')
+
+  function handleTitle (evt) {
+    setTitle(evt.target.value)
+  }
+
+  function handleContent (evt) {
+    setContent(evt.target.value)
+  }
+
+  function handleCreate (evt) {
+    evt.preventDefault()
+
+    const newPost = { title, content, author: user }
+    setPosts([ newPost, ...posts ])
+    setTitle('')
+    setContent('')
+  }
+
   return (
-    <form onSubmit={e => e.preventDefault()}>
+    <form onSubmit={handleCreate}>
       <div>Author: <b>{user}</b></div>
       <div>
         <label htmlFor="create-title">Title:</label>
-        <input type="text" name="create-title" id="create-title" />
+        <input
+          type="text"
+          name="create-title"
+          id="create-title"
+          value={title}
+          onChange={handleTitle}
+        />
       </div>
-      <textarea />
-      <input type="submit" value="Create" />
+      <textarea
+        value={content}
+        onChange={handleContent}
+      />
+      <input
+        type="submit"
+        value="Create"
+      />
     </form>
   )
 }
