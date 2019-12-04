c1dd42dde206d25921c4283cdea90f9c0df36240
Gustavo Saiani <gustavo@poe.ma>
2019-12-04 11:03:03 -0300

    Convert setUser to useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
@@ -16,13 +16,25 @@ const defaultPosts = [
   },
 ]
 
+function userReducer (state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+    case 'REGISTER':
+      return action.username
+    case 'LOGOUT':
+      return ''
+    default:
+      throw new Error('')
+  }
+}
+
 export default function App () {
-  const [ user, setUser ] = useState('')
+  const [ user, dispatchUser ] = useReducer(userReducer, '')
   const [ posts, setPosts ] = useState(defaultPosts)
 
   return (
     <div style={{ padding: 8 }}>
-      <UserBar user={user} setUser={setUser} />
+      <UserBar user={user} dispatch={dispatchUser} />
       <br />
       {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
