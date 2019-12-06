d58b80f9d62410d21a23d32169ec2e4c71c8e1f0
Gustavo Saiani <gustavo@poe.ma>
2019-12-06 14:04:29 -0300

    Add header with Context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useReducer, useEffect } from 'react'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
+import Header from './Header'
+import { ThemeContext } from './contexts'
 
 import appReducer from './reducers'
 
@@ -18,20 +20,21 @@ const defaultPosts = [
   },
 ]
 
-export default function App () {
+export default function App ({projectName}) {
   const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
   const {user, posts} = state
 
   useEffect(() => {
     if (user) {
-      document.title = `${user} — React Hooks Blog`
+      document.title = `${user} — ${projectName}`
     } else {
-      document.title = `React Hooks Blog`
+      document.title = projectName
     }
-  }, [user])
+  }, [projectName, user])
 
   return (
     <div style={{ padding: 8 }}>
+      <Header text={projectName} />
       <UserBar user={user} dispatch={dispatch} />
       <br />
       {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
