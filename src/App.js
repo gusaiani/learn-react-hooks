36f0d52405fa54ab6fe06fc3b915e3fa75868d48
Gustavo Saiani <gustavo@poe.ma>
2020-01-22 10:13:45 -0300

    Install navi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useReducer, useEffect, useState } from 'react'
+import { Router, View } from 'react-navi'
+import { mount, route } from 'navi'
 import { ThemeContext, StateContext } from './contexts'
 import HeaderBar from './pages/HeaderBar'
 import HomePage from './pages/HomePage'
+import PostPage from './pages/PostPage'
 
 import appReducer from './reducers'
+
+const routes = mount({
+  '/': route({ view: <HomePage /> }),
+  '/view/:id': route(req => {
+    console.log({req})
+    return { view: <PostPage id={req.params.id} /> }
+  }),
+})
 
 export default function App ({projectName}) {
   const [ theme, setTheme ] = useState('')
   const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: [], error: '' })
-  const {user, error} = state
+  const {user} = state
 
   useEffect(() => {
     if (user) {
@@ -22,11 +33,13 @@ export default function App ({projectName}) {
   return (
     <StateContext.Provider value={{ state, dispatch }}>
       <ThemeContext.Provider value={theme}>
-        <div style={{ padding: 8 }}>
-          <HeaderBar setTheme={setTheme} projectName={projectName} />
-          <hr />
-          <HomePage />
-        </div>
+        <Router routes={routes}>
+          <div style={{ padding: 8 }}>
+            <HeaderBar setTheme={setTheme} projectName={projectName} />
+            <hr />
+            <View />
+          </div>
+        </Router>
       </ThemeContext.Provider>
     </StateContext.Provider>
   )
